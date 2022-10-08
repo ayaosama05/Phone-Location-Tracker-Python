e5ec34689bc3b6f0f753c79131521264409652b0
Aya Osama <ayaosama0508@gmail.com>
2022-10-08 11:24:29 +0200

    Handle Errors

--- data.py
+++ data.py
@@ -1,1 +1,3 @@
-target_phone_number = "+201000000000"
+#target_phone_number = "+201000000000"
+
+target_phone_number = input("Enter Phone Number with code : ")
--- main.py
+++ main.py
@@ -5,22 +5,30 @@ from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
 
-#get phone number details
-phone_number_detailed = phonenumbers.parse(target_phone_number)
-print(phone_number_detailed)
+try:
+    # get phone number details
+    phone_number_detailed = phonenumbers.parse(target_phone_number)
+    print(phone_number_detailed)
 
-#get phone number country location
+    valid = phonenumbers.is_valid_number(phone_number_detailed)
 
-ch_number = phonenumbers.parse(target_phone_number, "CH")
-country_en = geocoder.description_for_number(ch_number, "en")
-country_ar = geocoder.description_for_number(ch_number, "ar")
-print("Country is {0} {1}".format(country_en, country_ar))
+    if valid is not True:
+        print("Please make sure that you enter the code following by phone number correctly")
+    else:
+        # get phone number country location
 
-#get phone number service provider
-ro_number = phonenumbers.parse(target_phone_number, "ro")
-provider_name_en = carrier.name_for_number(ro_number, "en")
-print("Service Provider is {0}".format(provider_name_en))
+        ch_number = phonenumbers.parse(target_phone_number, "CH")
+        country_en = geocoder.description_for_number(ch_number, "en")
+        country_ar = geocoder.description_for_number(ch_number, "ar")
+        print("Country is {0} {1}".format(country_en, country_ar))
 
-#get phone number timeZone
-phone_number_timezone = timezone.time_zones_for_number(phone_number_detailed)
-print("Time Zone is {0}".format(phone_number_timezone))
+        # get phone number service provider
+        ro_number = phonenumbers.parse(target_phone_number, "ro")
+        provider_name_en = carrier.name_for_number(ro_number, "en")
+        print("Service Provider is {0}".format(provider_name_en))
+
+        # get phone number timeZone
+        phone_number_timezone = timezone.time_zones_for_number(phone_number_detailed)
+        print("Time Zone is {0}".format(phone_number_timezone))
+except:
+    print("Invalid Phone Number format")
