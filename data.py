4d14f79a3362cfc27e720326f5e1c02ada8bfffa
ayaosama05 <ayaosama0508@gmail.com>
2022-10-08 10:37:28 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,1 @@
+target_phone_number = "+201000000000"
---
+++ main.py
@@ -0,0 +1,26 @@
+import phonenumbers
+from data import target_phone_number
+
+from phonenumbers import geocoder
+from phonenumbers import carrier
+from phonenumbers import timezone
+
+#get phone number details
+phone_number_detailed = phonenumbers.parse(target_phone_number)
+print(phone_number_detailed)
+
+#get phone number country location
+
+ch_number = phonenumbers.parse(target_phone_number, "CH")
+country_en = geocoder.description_for_number(ch_number, "en")
+country_ar = geocoder.description_for_number(ch_number, "ar")
+print("Country is {0} {1}".format(country_en, country_ar))
+
+#get phone number service provider
+ro_number = phonenumbers.parse(target_phone_number, "ro")
+provider_name_en = carrier.name_for_number(ro_number, "en")
+print("Service Provider is {0}".format(provider_name_en))
+
+#get phone number timeZone
+phone_number_timezone = timezone.time_zones_for_number(phone_number_detailed)
+print("Time Zone is {0}".format(phone_number_timezone))
